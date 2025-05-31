5bdeb17b5ae2985372f110c13eec1d3f9dbb10c2
Sania220107 <ristasania22@gmail.com>
2025-05-31 11:49:54 +0700

    first commit

--- index.js
+++ index.js
@@ -39,11 +39,13 @@ app.use("/uploads", express.static("uploads"));
 // Mengatur sinkronisasi database
 const syncDb = process.env.DB_SYNC === "true"; // Mengambil nilai dari environment
 
+const PORT = process.env.PORT || 5000;
+
 sequelize
   .sync({ force: false }) // Menggunakan force: false untuk tidak menghapus data saat sinkronisasi
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server berjalan pada port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server berjalan pada port ${PORT}`);
       if (syncDb) {
         console.log("Database telah disinkronisasi.");
       } else {
